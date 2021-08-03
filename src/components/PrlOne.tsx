80d9756d104afded445f971aadd503f1a5d9a25e
Serkan Akman <akmanserkan65@gmail.com>
2021-08-03 19:55:57 +0300

    Second commit

--- src/components/PrlOne.tsx
+++ src/components/PrlOne.tsx
@@ -2,7 +2,7 @@ import { Parallax, Background } from "react-parallax";
 
 const ParallaxOne = () => (
   <div className="image">
-    <Parallax blur={3} bgImage="ape.jpg" bgImageAlt="the cat" strength={400}>
+    <Parallax blur={6} bgImage="ape.jpg" bgImageAlt="the cat" strength={-200}>
       <div className="inner" />
     </Parallax>
     <Parallax
@@ -17,18 +17,14 @@ const ParallaxOne = () => (
       bgImage="lion.jpg"
       renderLayer={(percentage) => (
         <div
+          className="elephant"
           style={{
-            position: "absolute",
-            background: `rgba(255, 125, 0, ${percentage * 1})`,
-            left: "50%",
-            top: "50%",
-            borderRadius: '50%',
-            overflow: 'hidden',
-            transform: 'translate(-50% , -50%)',
             width: percentage * 500,
             height: percentage * 500,
           }}
-        ><img className="baby" src="baby.jpg" alt="baby" /></div>
+        >
+          <img className="baby" src="baby.jpg" alt="baby" />
+        </div>
       )}
     >
       <div className="inner"></div>
